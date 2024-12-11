74bd9d77c9ecb8e9e3a3a563f9b7b10d1d199ef5
vostakov <yanik135790@gmail.com>
2024-12-11 14:25:11 +0300

    Step to ML

--- train.py
+++ train.py
@@ -4,3 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 data = pd.read_csv('train_filtered.csv')
+
+X = data.drop(columns=['target']) # Признаки
+Y = data['target'] # То, что нам нужно
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+model = lightgbm.LGBMRegressor(random_state=42)
+model.fit(X_train, Y_train)
